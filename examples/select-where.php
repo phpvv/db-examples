c92df2685c0845a4f44e64fdc1138f77871de146
Volodymyr Sarnytskyi <v00v4n@gmail.com>
2021-09-18 23:51:58 +0300

    improves \VV\Db\Sql\Condition

---
+++ examples/select-where.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the VV package.
+ *
+ * (c) Volodymyr Sarnytskyi <v00v4n@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace examples;
+
+require __DIR__ . '/../bootstrap.php';
+
+use App\Db\MainDb;
+use VV\Db\Sql;
+
+$db = MainDb::instance();
+
+$query = $db->tbl->product->select(/*...*/)
+    ->where(                                // WHERE
+        Sql::condition()
+            ->and('color_id')->eq(5)            // "color_id" = ?
+            ->and('price')->lte(2000)           // AND "price" <= ?
+            ->and('title')->isNotNull()         // AND "title" IS NOT NULL
+            ->and('brand_id')->in(2, 3)         // AND "brand_id" IN (?, ?)
+            ->and('width')->between(250, 350)   // AND "width" BETWEEN ? AND ?
+            ->and('state=1')->custom()          // AND state=1 -- w/o quotes: custom query not changed
+    );
+
+echo $query->toString(), "\n";
+print_r($query->rows);
+
+$query = $db->tbl->product->select()
+    ->where('color_id', 5)      // same: `->where('color_id =', 5)`
+    ->where('price <=', 2000)   // supported operators: = | != | <> | < | > | <= | >=
+    ->where('title !=', null);
+
+echo $query->toString(), "\n";
+print_r($query->rows);
+
+
+$query = $db->tbl->product->select()
+    ->where('`width` BETWEEN ? AND ?', [250, 350])  // custom sql with binding parameters
+    ->where('state=1');                             // custom sql w/o binding parameters
+
+echo $query->toString(), "\n";
+print_r($query->rows);
+
+
+$query = $db->tbl->product->select()
+    ->where([
+        'color_id' => 5,
+        'price <=' => 2000,
+        'title !=' => null,
+        Sql::condition('brand_id')->eq(2)->or->eq(3),   // AND ("brand_id" = ? OR "brand_id" = ?)
+        '`width` BETWEEN ? AND ?' => [250, 350],
+        'state=1',
+    ]);
+
+echo $query->toString(), "\n";
+print_r($query->rows);
--- examples/t.php
+++ examples/t.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 
 /*
  * This file is part of the phpvv package.
@@ -8,37 +8,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
+
 namespace examples;
 
 require __DIR__ . '/../bootstrap.php';
 
 $db = \App\Db\MainDb::instance();
-
-$query = $db->tbl->product->select('product_id', 'price', 'weight')
-    ->addNestedColumns('brand', 'b.brand_id', 'b.title')
-    ->addNestedColumns(['nested', 'color'], 'c.color_id', 'c.title')
-    ->addNestedColumns(['nested', 'size'], 'width', 'height', 'depth')
-    ->join($db->tbl->brand)
-    ->join($db->tbl->color, 'p');
-
-print_r($query->row);
-
-
-$query = $db->tbl->orderItem->select('item_id', 'price', 'quantity')
-    ->joinNestedColumns(
-        $db->tbl->order->select('order_id', 'amount', 'comment'), // sub query
-        'oi.order_id',                                            // ON condition (see above)
-        ['my', 'nested', 'order']                                // nesting path
-    )
-    ->joinNestedColumns(
-        $db->tbl->product->select('product_id', 'title')          // sub query
-            ->joinNestedColumns(
-                $db->tbl->brand->select('brand_id', 'title'),     // sub sub query
-                'p.brand_id'
-            )
-            ->joinNestedColumns($db->tbl->color, 'p', 'color'),   // just join table - select all its columns
-        'oi.product_id',
-        'product'
-    );
-
-print_r($query->row);
